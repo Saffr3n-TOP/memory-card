88e92e6e194e7a42eaea5ea8b802724d99ee64d9
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2024-01-19 11:55:21 +0300

    Create constants.ts

---
+++ src/utils/constants.ts
@@ -0,0 +1,41 @@
+export const ROUND_CARDS_AMOUNT = 4;
+
+export const POKEAPI_URL = 'https://pokeapi.co/api/v2/pokemon/';
+
+export const POKEMON_NAMES: readonly string[] = [
+  'bulbasaur',
+  'charmander',
+  'squirtle',
+  'caterpie',
+  'pidgey',
+  'rattata',
+  'spearow',
+  'ekans',
+  'pikachu',
+  'sandshrew',
+  'clefairy',
+  'vulpix',
+  'jigglypuff',
+  'zubat',
+  'venonat',
+  'diglett',
+  'meowth',
+  'psyduck',
+  'mankey',
+  'growlithe',
+  'abra',
+  'machop',
+  'bellsprout',
+  'geodude',
+  'ponyta',
+  'slowpoke',
+  'magnemite',
+  'seel',
+  'marowak',
+  'chansey',
+  'horsea',
+  'staryu',
+  'eevee',
+  'gastly',
+  'hypno'
+];
